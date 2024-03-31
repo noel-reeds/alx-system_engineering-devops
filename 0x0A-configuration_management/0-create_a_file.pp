f97ca94e416b8e0d420cf0d3f958271194990d9f
altoid <noelreeds01@gmail.com>
2024-03-31 08:49:00 +0000

    added path to manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creates a file in /tmp
 file { '/tmp/school':
+    path    => '/tmp/school',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet\n',
+    content => 'I love Puppet',
 }
