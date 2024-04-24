bdb6783d8a931cc1814ace0824ad7897b80d4851
altoid <noelreeds01@gmail.com>
2024-04-24 07:51:46 +0000

    exports to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Retrieves data from an API"""
+
+import csv
+import requests
+import sys
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    for todo in todos:
+        if todo.get('userId') == employee_id:
+            for user in users:
+                if user.get('id') == employee_id:
+                    output = f"{user.get('id')},\
+{user.get('username')},{todo.get('completed')},\
+{todo.get('title')}\n"
+                    with open("output.txt", "a") as myFile:
+                        myFile.write(output)
+with open("output.txt", "r") as myFile:
+    stripped = (line.strip() for line in myFile)
+    splitted = (line.split(",") for line in myFile if line)
+    with open("USER_ID.csv", "w") as file3:
+        writer = csv.writer(file3)
+        writer.writerows(splitted)
