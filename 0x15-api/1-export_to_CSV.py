81f496d6b7892cf9a2005140149a1e8887c13be6
altoid <noelreeds01@gmail.com>
2024-04-24 22:41:00 +0000

    retrieves correct USER_ID and username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,20 +8,16 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     users = requests.get('https://jsonplaceholder.typicode.com/users').json()
-    rows = []
-
+    row = []
+    for user in users:
+        if user.get('id') == employee_id:
+            username = user.get('username')
+            break
     for todo in todos:
         if todo.get('userId') == employee_id:
-            for user in users:
-                if user.get('id') == employee_id:
-                    output = f"{user.get('id')},\
-{user.get('username')},{todo.get('completed')},{todo.get('title')}\n"
-                    with open("output.txt", "a") as myFile:
-                        myFile.write(output)
-    with open("output.txt", "r") as myFile:
-        stripped = (line.strip() for line in myFile)
-        splitted = (line.split(",") for line in stripped if line)
+                    row.append([employee_id,username,\
+todo.get('completed'),todo.get('title')])
         filename = f"{employee_id}.csv"
-        with open(filename, "w") as file3:
+        with open(filename, mode="w", encoding='utf-8') as file3:
             writer = csv.writer(file3, quoting=csv.QUOTE_ALL)
-            writer.writerows(splitted)
+            writer.writerows(row)
