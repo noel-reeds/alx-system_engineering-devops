c49667e2bad7aa681104502fbb906f19a4fba9fc
altoid <noelreeds01@gmail.com>
2024-04-24 22:03:46 +0000

    Fix: identation err

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,18 +8,20 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    rows = []
+
     for todo in todos:
         if todo.get('userId') == employee_id:
             for user in users:
                 if user.get('id') == employee_id:
                     output = f"{user.get('id')},\
-{user.get('username')},{todo.get('completed')},\
-{todo.get('title')}\n"
+{user.get('username')},{todo.get('completed')},{todo.get('title')}\n"
                     with open("output.txt", "a") as myFile:
                         myFile.write(output)
-with open("output.txt", "r") as myFile:
-    stripped = (line.strip() for line in myFile)
-    splitted = (line.split(",") for line in stripped if line)
-    with open("USER_ID.csv", "w") as file3:
-        writer = csv.writer(file3, quoting=csv.QUOTE_ALL)
-        writer.writerows(splitted)
+    with open("output.txt", "r") as myFile:
+        stripped = (line.strip() for line in myFile)
+        splitted = (line.split(",") for line in stripped if line)
+        filename = f"{employee_id}.csv"
+        with open(filename, "w") as file3:
+            writer = csv.writer(file3, quoting=csv.QUOTE_ALL)
+            writer.writerows(splitted)
