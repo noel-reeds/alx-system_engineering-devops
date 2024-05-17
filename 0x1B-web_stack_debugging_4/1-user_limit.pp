d3c9aeaa42f2110110595f7aaa806ede7223faa7
altoid <noelreeds01@gmail.com>
2024-05-17 20:43:31 +0000

    manifest to execute with root priviledges

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,9 @@
 # adjusts user login limits
-exec {'change_os_configuration_for_holberton_user':
-  command => 'sed -i "s/5/2048/" etc/security/limits.conf; sed -i "s/4/1024/" etc/security/limits.conf',
+exec {'change_os_configuration_for_holberton_user1':
+  command => 'sudo sed -i "s/nofile 5/nofile 2048/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
-  user    => 'root',
+}
+exec {'change_os_configuration_for_holberton_user':
+  command => 'sudo sed -i "s/nofile 4/nofile 1024/" etc/security/limits.conf',
+  path    => ['usr/bin', '/bin'],
 }
