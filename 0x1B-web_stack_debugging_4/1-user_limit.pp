5cd5185933e5d3ef9cdd9d1e7e5032f9c47322da
altoid <noelreeds01@gmail.com>
2024-05-17 21:25:33 +0000

    specified user in manifest

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,12 @@
-# adjusts user login limits
+# adjusts OS config for holberton user
 exec {'change_os_configuration_for_holberton_user1':
-  command => 'sudo sed -i "/holberton hard/s/5/2048/" etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/2048/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
+  user    => 'root',
   before  => Exec['change_os_configuration_for_holberton_user'],
 }
 exec {'change_os_configuration_for_holberton_user':
-  command => 'sudo sed -i "/holberton soft/s/4/1024/" etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/1024/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
+  user    => 'root',
 }
