a8aec67e94a20265e9e5666877c530ad3e5377af
altoid <noelreeds01@gmail.com>
2024-05-17 18:36:36 +0000

    changed permissions

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,4 +2,5 @@
 exec {'change_os_configuration_for_holberton_user':
   command => 'sed -i "s/5/2048/" etc/security/limits.conf; sed -i "s/4/1024/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
+  user    => 'root',
 }
