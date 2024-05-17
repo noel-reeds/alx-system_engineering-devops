03cb1a0a05b4840dc3ffa9e62c81118aaefe3bc5
altoid <noelreeds01@gmail.com>
2024-05-17 20:58:53 +0000

    narrowed search for target string

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # adjusts user login limits
 exec {'change_os_configuration_for_holberton_user1':
-  command => 'sudo sed -i "s/nofile 5/nofile 2048/" etc/security/limits.conf',
+  command => 'sudo sed -i "/holberton hard/s/5/2048/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
 }
 exec {'change_os_configuration_for_holberton_user':
-  command => 'sudo sed -i "s/nofile 4/nofile 1024/" etc/security/limits.conf',
+  command => 'sudo sed -i "/holberton soft/s/4/1024/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
 }
