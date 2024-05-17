a9a2bfa0da99a34646c9ae036bf89693b59393d1
altoid <noelreeds01@gmail.com>
2024-05-17 22:01:39 +0000

    sighs

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,6 @@
-# adjusts OS config for holberton user
-exec {'change_os_configuration_for_holberton_user1':
-  command => 'sed -i "/holberton hard/s/5/2048/" etc/security/limits.conf',
-  path    => ['usr/bin', '/bin'],
-  user    => 'root',
-  before  => Exec['change_os_configuration_for_holberton_user'],
-}
+# adjusts OS configs for holberton user
 exec {'change_os_configuration_for_holberton_user':
-  command => 'sed -i "/holberton soft/s/4/1024/" etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/2048/" etc/security/limits.conf; sed -i "/holberton soft/s/4/1024/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
   user    => 'root',
 }
