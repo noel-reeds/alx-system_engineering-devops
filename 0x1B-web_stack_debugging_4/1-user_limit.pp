426f7075c3316e4c3306f91077f7436d34088e0a
altoid <noelreeds01@gmail.com>
2024-05-17 21:18:10 +0000

    added sequential order

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 exec {'change_os_configuration_for_holberton_user1':
   command => 'sudo sed -i "/holberton hard/s/5/2048/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
-  require => Exec['change_os_configuration_for_holberton_user'],
+  before => Exec['change_os_configuration_for_holberton_user'],
 }
 exec {'change_os_configuration_for_holberton_user':
   command => 'sudo sed -i "/holberton soft/s/4/1024/" etc/security/limits.conf',
