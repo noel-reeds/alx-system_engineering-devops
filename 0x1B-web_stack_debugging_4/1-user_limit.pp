87298815b29d8b538f95791f9c3a46f9fac12626
altoid <noelreeds01@gmail.com>
2024-05-17 14:28:42 +0000

    a diff pattern

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,9 @@
-# adjusts user limitations.
 exec {'update_hard_limit':
-  command => 'sed -i "s/nofile 5/nofile 2048/" etc/security/limits.conf',
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 2048/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
 }
 
 exec {'update_soft_limit':
-  command => 'sed -i "s/nofile 4/nofile 1024/" etc/security/limits.conf',
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 1024/" etc/security/limits.conf',
   path    => ['usr/bin', '/bin'],
 }
