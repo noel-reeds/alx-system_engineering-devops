9a678b34e101345c6808bd8bbb3ff850ec83f05e
altoid <noelreeds01@gmail.com>
2024-05-18 10:58:20 +0000

    sorts puppet-lint validation

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # adjusts OS configs for holberton user
 exec {'change-os-configs-for-holberton-user':
-  command  => 'sudo sed -i "/s/nofile 5/nofile 2048/" /etc/security/limits.conf',
-  path     => ['/usr/bin/', '/bin/'],
+  command => 'sudo sed -i "/s/nofile 5/nofile 2048/" /etc/security/limits.conf',
+  path    => ['/usr/bin/', '/bin/'],
 }
 exec {'change-os-configs-for-holberton-user2':
-  command  => 'sudo sed -i "/s/nofile 4/nofile 2048/" /etc/security/limits.conf',
-  path     => ['/usr/bin/', '/bin/'],
+  command => 'sudo sed -i "/s/nofile 4/nofile 2048/" /etc/security/limits.conf',
+  path    => ['/usr/bin/', '/bin/'],
 }
