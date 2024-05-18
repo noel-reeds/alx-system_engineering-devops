ed1709d02c552b982c56b6ee9516b9f225f7eff0
altoid <noelreeds01@gmail.com>
2024-05-18 11:03:22 +0000

    sorts syntax err

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # adjusts OS configs for holberton user
 exec {'change-os-configs-for-holberton-user':
   provider => shell,
-  command  => 'sudo sed -i "/s/nofile 5/nofile 2048/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 5/nofile 2048/" /etc/security/limits.conf',
 }
 exec {'change-os-configs-for-holberton-user2':
   provider => shell,
-  command  => 'sudo sed -i "/s/nofile 4/nofile 2048/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 4/nofile 2048/" /etc/security/limits.conf',
 }
